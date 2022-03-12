6b158616b368aec3ed5f20e46aa1b588fe030119
Leo Merlo <lmerlo@perceptyx.com>
2022-03-12 00:19:31 -0300

    Separate records by user

--- gerardo/src/App.vue
+++ gerardo/src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="app">
     <router-view/>
+    <button
+      v-if="isUserLogged"
+      @click="signOutUser"
+    >
+      Sign out
+    </button>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'App',
-  created() {
-    this.$store.dispatch('setExpenses')
+  computed: {
+    ...mapGetters([
+      'getUser',
+    ]),
+    isUserLogged() {
+      return this.getUser?.uid;
+    }
+  },
+  methods: {
+    ...mapActions([
+      'signOutUser',
+    ])
   }
 }
 </script>
--- gerardo/src/router/index.js
+++ gerardo/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import routes from './routes';
+import store from '@/store';
 
 Vue.use(VueRouter);
 
@@ -9,5 +10,18 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (store.getters.getUser) {
+      next()
+      return
+    } else {
+      next('/login').catch(()=>{});
+    }
+  } else {
+    next()
+  }
+})
 
 export default router;
