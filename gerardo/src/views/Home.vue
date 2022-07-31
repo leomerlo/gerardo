18538ce12b9a2b8efed5def275af8e056f63ec9d
Leo Merlo <merloleandro@gmail.com>
2022-07-31 11:30:30 -0300

    Updated UI
    - New view for monthly expenses
    - Toggle for recurrent expenses

--- gerardo/src/views/Home.vue
+++ gerardo/src/views/Home.vue
@@ -1,61 +1,12 @@
-<template>
-  <div>
-    <h1>Gerardo</h1>
-
-    <monthly-expense
-      :month="getMonth"
-      :key="getMonth"
-    />
-
-    <button @click="prevMonth"> Atras </button>
-    <button @click="nextMonth"> Adelante </button>
-
-    <button @click="clearData"> Limpiar Data </button>
-
-  </div>
-</template>
-
 <script>
-import { mapGetters } from 'vuex';
+import router from '../router'
 import moment from 'moment';
-import monthlyExpense from '@/components/monthlyExpense';
 
 export default {
-  name: 'Home',
-  components: {
-    monthlyExpense,
-  },
-  data() {
-    return {
-      monthTracker: 0,
+    name: 'Home',
+    created() {
+        const currentMonth = parseInt(moment(new Date).add(this.monthTracker, 'months').format('M'));
+        router.push({ name: 'Monthly', params: { month: currentMonth }})
     }
-  },
-  computed: {
-    ...mapGetters([
-      'allExpenses',
-    ]),
-    getMonth() {
-      return parseInt(moment(new Date).add(this.monthTracker, 'months').format('M'))
-    }
-  },
-  methods: {
-    nextMonth(){
-      this.monthTracker++;
-    },
-    prevMonth(){
-      this.monthTracker--;
-    },
-    clearData() {
-      this.allExpenses.filter((e) => {
-        if(e.recurrentId) {
-          this.$store.dispatch('deleteExpense',e.id);
-        }
-      })
-    }
-  },
 }
 </script>
-
-<style>
-
-</style>
